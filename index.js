3e22752a1a09d9786a09737348eed3f18fbbaee3
Zoe Mendez <zoemendez316@gmail.com>
2022-07-18 20:23:12 -0600

    commit tree 8afef4b1cab0aa9e284de26871eccaf37d0bc166

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(event = 'go to the office') {
+    return `This Monday, I will ${event}.`
+}
+
+function wrapAdjective(visual) {
+    return function (help = 'special') {
+        return `You are ${visual}${help}${visual}!`
+    }
+}
